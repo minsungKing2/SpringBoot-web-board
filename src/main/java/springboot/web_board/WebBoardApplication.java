ced2c203e7a9e59da55d6c7218d08c5a2cd72bdd
MinSung Jung <minpro22@naver.com>
2025-07-21 17:10:38 +0900

    first commit

---
+++ src/main/java/springboot/web_board/WebBoardApplication.java
@@ -0,0 +1,13 @@
+package springboot.web_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBoardApplication.class, args);
+		//테스트111111111
+	}
+}
