936b36f1584bdbfe5469e5fc4905a348809611a8
MinSung Jung <minpro22@naver.com>
2025-07-21 17:12:58 +0900

    111111

--- src/main/java/springboot/web_board/WebBoardApplication.java
+++ src/main/java/springboot/web_board/WebBoardApplication.java
@@ -8,6 +8,6 @@ public class WebBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBoardApplication.class, args);
-		//테스트111111111
+		//테스트1111111111111
 	}
 }
