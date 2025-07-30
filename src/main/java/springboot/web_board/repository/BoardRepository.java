80794b5fdb175c1299adc31afb81143c400f2f4d
MinSung Jung <minpro22@naver.com>
2025-07-30 17:09:28 +0900

    25.07.30 web_board commit

--- src/main/java/springboot/web_board/repository/BoardRepository.java
+++ src/main/java/springboot/web_board/repository/BoardRepository.java
@@ -14,10 +14,11 @@ public class BoardRepository {
 
     private final SqlSessionTemplate sql; // mybatis 를 사용하기 위한 SqlSessionTemplate 객체 사용
 
-    public void save(BoardDTO boardDTO) {
+    public BoardDTO save(BoardDTO boardDTO) {
         sql.insert("Board.save", boardDTO);
         // insert 메서드의 파라미터로 mapper 의 namespace 인 Board 로 접근하여 sql 문 수행
         // board-mapper.xml 에서 해당 mapper 의 namespace 를 Board 로 지정해놓았음.
+        return boardDTO;
     }
 
     public List<BoardDTO> findAll(){
